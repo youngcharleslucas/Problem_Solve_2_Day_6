61c670ec1c22839f0815009a88d3e86d089dfeb3
Your Name <charles.luas.young@gmail.com>
2021-10-08 14:06:57 -0600

    Initial

---
+++ main.py
@@ -0,0 +1,36 @@
+'''
+
+To be a good problem solver, it is important to be able to break problems down. One way to go about this is to write out 
+the steps it will take to solve the problem. These steps are written down in English in a manner that are easily 
+explainable to someone who may not be technical. The idea is that in order to code something out, you first need to 
+have a good understanding of what it is you are attempting to solve.
+
+For each of the three problem solving problems below, write out the steps it will take to go about solving the problem. 
+For example, once you are done writing out the steps for the “happy numbers” problem, you would then write out the code 
+to solve the problem. You would then repeat the process for each ensuing problem. Ideally, this will be a good habit 
+that you will develop and carry forward with you for all problems you encounter at devCodeCamp and beyond.
+
+1. Happy Numbers a. https://en.wikipedia.org/wiki/Happy_number
+
+    b. A happy number is a number defined by the following process: starting with any positive integer, replace the number 
+        by the sum of the squares of its digits, and repeat the process until the number equals 1. An example of a happy number is 19
+
+    c. Write a method that determines if a number is happy or sad
+
+2. Prime Numbers
+
+    a. A prime number is a number that is only divisible by one and itself.
+
+    b. Write a method that prints out all prime numbers between 1 and 100
+
+3. Fibonacci
+
+    a. A series of numbers in which each number (Fibonacci number) is the sum of the two preceding numbers. The simplest is the 
+        series 1, 1, 2, 3, 5, 8, etc.
+
+    b. Write a method that does the Fibonacci sequence starting at 1
+
+    c. HARDER VERSION: Write a method that does the Fibonacci sequence starting at a number that a user inputs
+
+'''
+
